8a9a0a5d6eb1cbe0201b5cea2408077fc2472115
Lina Yaroslavski <lina.yrsl@gmail.com>
2019-07-11 15:52:33 +0300

    Adds new route

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from typing import Dict
 
+from flask import Flask, jsonify
 
 application = Flask(__name__)
 
@@ -7,6 +8,16 @@ application = Flask(__name__)
 @application.route("/hello/<name>")
 def hello_world(name: str):
     return "Hello, {}".format(name)
+
+
+@application.route("/personal-info")
+def personal_info(name: str):
+    info: Dict = {"first_name": "{}".format(name),
+                  "last_name": "yaroslavski",
+                  "email": "lina@mail.com"}
+
+    resp = jsonify(info)
+    return resp
 
 
 if __name__ == "__main__":
